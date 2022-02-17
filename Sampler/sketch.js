47b9673b3fc76c44b9a7ec5fbd94230df17ce230
Logan-Lafauci <78313635+Logan-Lafauci@users.noreply.github.com>
2022-02-17 13:44:53 -0600

    fixed sampler bugs

--- Sampler/sketch.js
+++ Sampler/sketch.js
@@ -1,11 +1,11 @@
-const sounds = new Tone.Players(
+let sounds = new Tone.Players(
   {
-    'snare': 'sample/snare.wav',
-    'kick': 'sample/kick.wav',
-    'hat': 'sample/hi-hat.wav',
-    'clap': 'clap1.mp3'
+    'snare': '/sample/snare.wav',
+    'kick': '/sample/kick.wav',
+    'hat': '/sample/hi-hat.wav',
+    'clap': '/sample/clap.wav'
   }
-)
+);
 let button1;
 let button2;
 let button3;
@@ -14,26 +14,35 @@ let button4;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   sounds.toDestination();
+  
 
   button1 = createButton('snare');
-  button1.position(100, 100);
-  button1.mousePressed(playSample('clap'));
+  button1.position(10, 10);
+  button1.size(width/2-15, height/ 2-20);
+  button1.style('font-size', '48px');
+  button1.mousePressed(() => playSample('snare'));
 
-  // button2 = createButton('kick');
-  // button2.position(300, 100);
-  // button2.mousePressed(playSample('kick'));
+  button2 = createButton('kick');
+  button2.position(width/2 +5, 10);
+  button2.size(width/2 - 15, height/ 2 - 20);
+  button2.style('font-size', '48px');
+  button2.mousePressed(() => playSample('kick'));
 
-  // button3 = createButton('hi-hat');
-  // button3.position(500, 100);
-  // button3.mousePressed(playSample('hat'));
+  button3 = createButton('hi-hat');
+  button3.position(10, height/ 2);
+  button3.size(width/2 - 15, height/ 2 - 20);
+  button3.style('font-size', '48px');
+  button3.mousePressed(() => playSample('hat'));
 
-  // button4 = createButton('clap');
-  // button4.position(700, 100);
-  // button4.mousePressed(playSample('clap'));
+  button4 = createButton('clap');
+  button4.position(width/2 +5, height/ 2);
+  button4.size(width/2 - 15, height/ 2 - 20);
+  button4.style('font-size', '48px');
+  button4.mousePressed(() => playSample('clap'));
 }
 
 function draw() {
-  background(255);
+  background(10, 225, 245);
 }
 
 function playSample(sound)
